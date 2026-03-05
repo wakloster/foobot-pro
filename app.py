f3961735b826bcf80bb9db511b98a88a4f007c39
Wesley <wesleikloster@gmail.com>
2026-03-05 16:38:56 -0300

    Adicionado mais uma API

--- app.py
+++ app.py
@@ -45,9 +45,10 @@ else:
 
 # --- CONFIGURAÇÕES DA API (SISTEMA DE FAILOVER) ---
 API_KEYS = [
+    "74d794123dbe38caf1f24a487feccb4b", #Chave do Eliabe
     #"7ae631412b052dece78c1876932d3c92", # CHAVE NOVA (Primária)
     #"c529d0695b02fa73ccdcc19cb89026d7",  # CHAVE ANTIGA (Backup)
-    "66dbfe1207c3fd57847c4ce39ecfcca9"
+    # "66dbfe1207c3fd57847c4ce39ecfcca9"
 ]
 
 def fazer_requisicao(url, params=None):
